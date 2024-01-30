1659372ad27af8104b05368ca2bb5f137486b2ff
Harshil <kumarharshil2001@gmail.com>
2024-01-30 15:20:30 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+import sklearn
+
+app=FastAPI()
+
+class PredictItem(BaseModel):
+    age: int
+    sex: int
+    cp: int
+    trestbps: int
+    chol: int
+    fbs: int
+    restecg: int
+    thalach: int
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.post('/')
+async def prediction(item: PredictItem):
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+    yhat = model.predict(df)
+    return {'prediction': int(yhat)}
